7f57a6b03bc0c0c6709ba55fa40fd3d8e6892c1d
Pige <goldenpig.2709@gmail.com>
2023-12-16 12:06:36 -0600

    actualizar interfaces Raul e implementar partes de las interfaces
    en este commit se actualizan las interfaces de Raul a la version mas reciente, al igual que logre implementar dichas interfaces a el codigo (parcialmente).

--- ProyectoOrdinario/ProyectoOrdinario/Interfaces/IJuego.cs
+++ ProyectoOrdinario/ProyectoOrdinario/Interfaces/IJuego.cs
@@ -4,9 +4,10 @@ namespace ProyectoOrdinario.Interfaces
 	public interface IJuego
 	{
 		IDealer Dealer { get; }
-		IComparadorDeManos ComparadorDeManos { get; }
+		bool JuegoTerminado { get; }
 		void AgregarJugador(IJugador jugador);
 		void IniciarJuego();
+		void JugarRonda();
 		void MostrarGanador();
 	}
 }
--- ProyectoOrdinario/ProyectoOrdinario/Program.cs
+++ ProyectoOrdinario/ProyectoOrdinario/Program.cs
@@ -1,9 +1,99 @@
-﻿namespace ProyectoOrdinario;
-class Program
+﻿using ProyectoOrdinario.Enumeradores;
+using ProyectoOrdinario.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace ProyectoOrdinario
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+
+        static void Main(string[] args)
+        {
+
+            List<Carta> DeckLista = new List<Carta>();
+
+            for (int i = 0; i <= 3; i++)
+            {
+                for (int j = 1; j <= 13; j++)
+                {
+                    DeckLista.Add(new Carta(j, i));
+                }
+            }
+
+            foreach (Carta c in DeckLista)
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+    public class DeckDeCartas : IDeckDeCartas
+    {
+        public void BarajearDeck()
+        {
+            Console.WriteLine("aa");
+        }
+
+        public ICarta VerCarta(int indiceCarta)
+        {
+            return new Carta(1,2);
+        }
+
+        public ICarta SacarCarta(int indiceCarta)
+        {
+            return new Carta(1,2);
+        }
+        public void MeterCarta(ICarta carta)
+        {
+
+        }
+        public void MeterCarta(List<Carta> DeckLista)
+        {
+            Console.WriteLine("");
+        }
+        public void MeterCarta(List<ICarta> cartas)
+        {
+
+        }
+
+    }
+    public class Carta : ICarta
+    {
+        private ValoresCartasEnum _valor;
+        public ValoresCartasEnum Valor
+        {
+            get { return _valor; }
+            set
+            {
+                if ((int)value < 1 || (int)value > 13) throw new Exception("El valor de la carta debe ser entre 1 y 13.");
+                _valor = value;
+            }
+        }
+
+        private FigurasCartasEnum _figura;
+        public FigurasCartasEnum Figura
+        {
+            get { return _figura; }
+            set
+            {
+                if ((int)value < 0 || (int)value > 3) throw new Exception("El valor de la figura debe ser entre 0 y 3.");
+                _figura = value;
+            }
+        }
+
+        public Carta(ValoresCartasEnum valor, FigurasCartasEnum figura)
+        {
+            this.Valor = valor;
+            this.Figura = figura;
+        }
+        public Carta(int valor, int figura)
+        {
+            this.Valor = (ValoresCartasEnum)valor;
+            this.Figura = (FigurasCartasEnum)figura;
+        }
+        public override string ToString()
+        {
+            return $"{this.Valor} de {this.Figura}";
+        }
     }
 }
-
